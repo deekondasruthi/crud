f9421157aea3586d797716c067c0a7ea98d62868
Gokilavanim06 <sruthideekonda2722@gmail.com>
2024-07-24 11:20:19 +0530

    update Code

---
+++ src/main/java/curd/BasicCurdApplication.java
@@ -0,0 +1,13 @@
+package curd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/curd/BasicCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package curd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
