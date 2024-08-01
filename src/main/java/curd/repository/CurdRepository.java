d1363eb73fc1499d6497cc1380c6467f1da0e544
Gokilavanim06 <sruthideekonda2722@gmail.com>
2024-08-01 15:44:34 +0530

    new update in pom.xml

--- src/main/java/curd/repository/CurdRepository.java
+++
@@ -1,9 +0,0 @@
-package curd.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import curd.entity.CurdEntity;
-
-public interface CurdRepository extends JpaRepository<CurdEntity, Integer>{
-
-}
